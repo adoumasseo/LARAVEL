744ade1e046ced17094a7242d28a116e755d90f2
adoumasseo <adoumasseo@gmail.com>
2024-10-01 12:26:26 +0100

    [feat]: Task CRUDs and endpoint

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,48 +3,69 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Models\Board;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Return a Json of all tasks
      */
     public function index()
     {
-        //
+        $tasks = Task::all();
+        return response()->json([
+            'tasks' => $tasks,
+        ], 200);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request, int $id)
     {
-        //
+        $board = Board::find($id);
+        if (!$board)
+        {
+            return response()->json([
+                "message" => "No task with that ID"
+            ], 404);
+        }
+        $validator = Validator::make($request->all(), [
+            'content' => "required|string|min:1",
+            'status' => 'sometimes|required|in:todo,doing,done'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        $task = Task::create([
+            'content' => $request->content,
+            'status' => $request->status ?? 'todo',
+            'board_id' => $board->id,
+        ]);
+        return response()->json([
+            'task' => $task,
+            'message' => 'task created successfully',
+        ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    public function show(int $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
-    {
-        //
+        $task = Task::find($id);
+        if (!$task)
+        {
+            return response()->json([
+                'message' => 'No Task with this ID found'
+            ], 404);
+        }
+        return response()->json([
+            'task' => $task
+        ]);
     }
 
     /**
@@ -52,14 +73,46 @@ class TaskController extends Controller
      */
     public function update(Request $request, Task $task)
     {
-        //
+        if (!$task)
+        {
+            return response()->json([
+                "message" => "No task found with this ID"
+            ]);
+        }
+        $validator = Validator::make($request->all(), [
+            'content' => "sometimes|required|string|min:1",
+            'status' => 'sometimes|required|in:todo,doing,done',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        if ($request->has('content')) {
+            $task->content = $request->content;
+        }
+        if ($request->has('status')) {
+            $task->status = $request->status;
+        }
+        $task->save();
+        return response()->json([
+            'message' => 'task updated successfully',
+            'task' => $task
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Task $task)
+    public function destroy(int $id)
     {
-        //
+        $task = Task::find($id);
+        if (!$task) {
+            return response()->json([
+                'message' => 'No task with this ID found'
+            ], 404);
+        }
+        $task->delete();
+        return response()->json([
+            'message' => 'task delete successfully'
+        ], 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BoardController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,20 +20,31 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// authentification
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 Route::middleware(['auth:sanctum', 'admin'])->group(function(){
+    // admin crud on users 
     Route::get('admin/dashboard', [UserController::class, 'index']);
     Route::put('admin/update-user/{user}', [UserController::class, 'update']);
     Route::delete('admin/delete-user/{user}', [UserController::class, 'destroy']);
 });
 Route::middleware('auth:sanctum')->group(function () {
-
+    // authentification
     Route::post('logout', [UserController::class, 'logout']);
+    //user profile actions
     Route::put('user/update-profile/{user}', [UserController::class, 'update_self']);
+    // cruds boards
     Route::get('user/boards', [BoardController::class, 'index']);
     Route::get('user/board-get-tasks/{board}', [BoardController::class, 'show']);
     Route::post('user/create-board', [BoardController::class, 'create']);
     Route::put('user/update-board/{board}', [BoardController::class, 'update']);
     Route::delete('user/delete-board/{board}', [BoardController::class, 'destroy']);
+    // cruds for tasks
+    Route::get('user/tasks', [TaskController::class, 'index']);
+    Route::get('user/show-task/{task}', [TaskController::class, 'show']);
+    Route::post('user/create-task', [TaskController::class, 'create']);
+    Route::put('user/update-task/{task}', [TaskController::class, 'update']);
+    Route::delete('user/delete-task/{task}', [TaskController::class, 'destroy']);
+
 });
